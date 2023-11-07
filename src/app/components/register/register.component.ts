6ea9c751fde5015fd1e6912928e2f48d58c561e3
Robert Morales <lsurec@demosoft.com.gt>
2023-11-06 21:41:35 -0600

    add register

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,10 +1,68 @@
 import { Component } from '@angular/core';
+import { RegisterInterface } from 'src/app/interfaces/register.interface';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+//Declaracion de variables a utilizar
+nombreInput: string = "";
+claveInput: string = "";
+confirmarClaveInput: string = "";
+saveMyData: boolean = false;
+mostrarTexto: boolean = false;
+isLoading: boolean = false;
+
+
+//Intancia de servicios
+constructor(
+  //Declaracion de variables privadas
+) {
+  
+}
+
+//guardar Token y navegar a la pantalla Home
+ngOnInit(): void {
+  // if (StorageService.token) {
+  //   this._router.navigate(['/station']);
+  // }
+}
+
+//Validar usuario y contraseña
+async login(): Promise<void> {
+
+  if (!this.nombreInput || !this.claveInput) {
+    alert ("Por favor completa todos los campos para continuar")
+    // this._widgetsService.openSnackbar("Por favor completa todos los campos para continuar");
+    return
+  }
+
+  //Interface de credenciales
+  let credenciales: RegisterInterface = {
+    user: this.nombreInput,
+    password: this.claveInput,
+    confirmPassword : this.confirmarClaveInput,
+  }
+
+  //TODO: login
+
+ 
+  if (this.saveMyData) {
+    //TODO:sesion permanente
+    //guardar el usuario
+    // StorageService.user = 'desa023';
+  }
+  else {
+    //TODO:sesion no permanente
+    
+    // sessionStorage.setItem('user', 'desa023');
+  }
+
+  //TODO:Si el usuario esta correcto
+  // this._router.navigate(['/home']);
+}
+
 
 }
---
+++ src/app/interfaces/register.interface.ts
@@ -0,0 +1,5 @@
+export interface RegisterInterface{
+    user:string;
+    password:string;
+    confirmPassword:string;
+}
