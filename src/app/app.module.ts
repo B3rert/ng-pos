bc32e2eb79f7a5728d4e5bfc549f238f8e9bf1bc
Robert Morales <lsurec@demosoft.com.gt>
2023-11-06 21:33:52 -0600

    add login design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
@@ -7,18 +8,21 @@ import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ProgressComponent } from './components/progress/progress.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    HomeComponent
+    HomeComponent,
+    ProgressComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    NgbModule
+    NgbModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,69 @@
 import { Component } from '@angular/core';
+import { LoginInterface } from 'src/app/interfaces/login.interface';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  //Declaracion de variables a utilizar
+  nombreInput: string = "";
+  claveInput: string = "";
+  saveMyData: boolean = false;
+  mostrarTexto: boolean = false;
+  isLoading: boolean = false;
 
+
+  //Intancia de servicios
+  constructor(
+    //Declaracion de variables privadas
+  ) {
+    
+  }
+
+  //guardar Token y navegar a la pantalla Home
+  ngOnInit(): void {
+    // if (StorageService.token) {
+    //   this._router.navigate(['/station']);
+    // }
+  }
+
+  //Validar usuario y contraseña
+  async login(): Promise<void> {
+
+    if (!this.nombreInput || !this.claveInput) {
+      alert ("Por favor completa todos los campos para continuar")
+      // this._widgetsService.openSnackbar("Por favor completa todos los campos para continuar");
+      return
+    }
+
+    //Interface de credenciales
+    let credenciales: LoginInterface = {
+      user: this.nombreInput,
+      password: this.claveInput,
+    }
+
+    //TODO: login
+
+   
+    if (this.saveMyData) {
+      //TODO:sesion permanente
+      //guardar el usuario
+      // StorageService.user = 'desa023';
+    }
+    else {
+      //TODO:sesion no permanente
+      
+      // sessionStorage.setItem('user', 'desa023');
+    }
+
+    //TODO:Si el usuario esta correcto
+    // this._router.navigate(['/home']);
+  }
+
+  //Permanencia de la sesión
+  rememberMe(): void {
+    this.saveMyData ? this.saveMyData = false : this.saveMyData = true;
+  }
 }
