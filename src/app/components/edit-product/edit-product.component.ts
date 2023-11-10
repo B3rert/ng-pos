744ac47eb2fa25c89009be3c6c2568163d67613c
Robert Morales <lsurec@demosoft.com.gt>
2023-11-09 21:55:02 -0600

    ad update product service

--- src/app/components/edit-product/edit-product.component.ts
+++ src/app/components/edit-product/edit-product.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { ProductInterface } from 'src/app/interfaces/product.interface';
+import { ResApiInterface } from 'src/app/interfaces/res-api.interface';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-edit-product',
   templateUrl: './edit-product.component.html',
-  styleUrls: ['./edit-product.component.css']
+  styleUrls: ['./edit-product.component.css'],
+  providers:[ProductService]
 })
 export class EditProductComponent {
+  constructor(private _productService: ProductService) {
 
+    this.putPorouct();
+
+  }
+
+  async putPorouct(){
+
+
+      let product:ProductInterface = {
+        "codigoProducto": "10001",
+        "nombreProducto": "producto prueba",
+        "precioUnitario": 9.5,
+        "cantidadProducto": 1
+    };
+
+    let res: ResApiInterface = await this._productService.putProduct(product);
+
+
+    if (!res.succes) {
+      console.error(res.response);
+
+      alert("Algo salio mal");
+    }
+
+
+    console.log(res.response);
+  }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -81,4 +81,41 @@ export class ProductService {
 
         });
     }
+
+
+     //funcion que va a realizar consumo privado
+     private _putProduct(product: ProductInterface) {
+        //configurar headers
+        let paramsStr = JSON.stringify(product); //JSON to String
+        let headers = new HttpHeaders({ "Content-Type": "application/json" });
+        //consumo de api
+        return this._http.put(`${this._urlBase}productos`, paramsStr, { headers: headers });
+    }
+
+    //funcion asyncrona con promise
+
+    putProduct(user: ProductInterface): Promise<ResApiInterface> {
+        //consumo primer servicio
+        return new Promise((resolve, reject) => {
+            this._putProduct(user).subscribe(
+                res => {
+                    let resApi: ResApiInterface = {
+                        succes: true,
+                        response: res
+                    }
+                    resolve(resApi)
+                },
+                //si algo sale mal
+                err => {
+                    let resApi: ResApiInterface = {
+                        succes: false,
+                        response: err
+                    }
+                    resolve(resApi);
+
+                }
+            );
+
+        });
+    }
 }
